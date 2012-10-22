57653b42553f0fda460f5db1861a086628ec1a36
zot <zot420@gmail.com>
2012-10-22 10:28:13 -0500

    [ap] new problem

---
+++ rosalind-consensus_and_profile.py
@@ -0,0 +1,14 @@
+import sys
+
+__author__ = 'apeckys'
+
+def __main():
+
+    file_name = sys.argv[1]
+    with open(file_name,'r') as f:
+        data = f.read()
+
+    matrix =
+
+if __name__ == '__main__':
+    __main()
